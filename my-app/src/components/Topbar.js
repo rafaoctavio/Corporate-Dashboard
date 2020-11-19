a707376714e836f20c73d79018546cd528860d24
rafaoctavio <rafaoctavio@gmail.com>
2020-11-19 17:34:57 -0300

    criando topbar

--- my-app/src/components/Topbar.js
+++ my-app/src/components/Topbar.js
@@ -1,47 +1,61 @@
-<div id="content-wrapper" class="d-flex flex-column">
+import React from 'react';
 
-			<!-- Main Content -->
-			<div id="content">
 
-				<!-- Topbar -->
-				<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+function Topbar (){
+    return (
+        <div id="content-wrapper" class="d-flex flex-column">
 
-					<!-- Sidebar Toggle (Topbar) -->
-					<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-						<i class="fa fa-bars"></i>
-					</button>
 
-					<!-- Topbar Navbar -->
-					<ul class="navbar-nav ml-auto">
+        <div id="content">
 
-						<!-- Nav Item - Alerts -->
-						<li class="nav-item dropdown no-arrow mx-1">
-							<a class="nav-link dropdown-toggle" href="/" id="alertsDropdown">
-								<i class="fas fa-bell fa-fw"></i>
-								<!-- Counter - Alerts -->
-								<span class="badge badge-danger badge-counter">3+</span>
-							</a>
-						</li>
+    
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-						<!-- Nav Item - Messages -->
-						<li class="nav-item dropdown no-arrow mx-1">
-							<a class="nav-link dropdown-toggle" href="/" id="messagesDropdown">
-								<i class="fas fa-envelope fa-fw"></i>
-								<!-- Counter - Messages -->
-								<span class="badge badge-danger badge-counter">7</span>
-							</a>
-						</li>
+                
+                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <i class="fa fa-bars"></i>
+                </button>
 
-						<div class="topbar-divider d-none d-sm-block"></div>
+                
+                <ul class="navbar-nav ml-auto">
 
-						<!-- Nav Item - User Information -->
-						<li class="nav-item dropdown no-arrow">
-							<a class="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span class="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
-								<img class="img-profile rounded-circle" src="assets/images/dummy-avatar.jpg" width="60">
-							</a>
-						</li>
+                    
+                    <li class="nav-item dropdown no-arrow mx-1">
+                        <a class="nav-link dropdown-toggle" href="/" id="alertsDropdown">
+                            <i class="fas fa-bell fa-fw"></i>
+                            
+                            <span class="badge badge-danger badge-counter">3+</span>
+                        </a>
+                    </li>
 
-					</ul>
+                    
+                    <li class="nav-item dropdown no-arrow mx-1">
+                        <a class="nav-link dropdown-toggle" href="/" id="messagesDropdown">
+                            <i class="fas fa-envelope fa-fw"></i>
+                            
+                            <span class="badge badge-danger badge-counter">7</span>
+                        </a>
+                    </li>
 
-				</nav>
+                    <div class="topbar-divider d-none d-sm-block"></div>
+
+                    
+                    <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="/" id="userDropdown">
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
+                            <img class="img-profile rounded-circle" src="assets/images/dummy-avatar.jpg" width="60"></img>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </nav>
+    </div>
+    </div>
+    
+
+
+    );
+}
+
+export default Topbar;
