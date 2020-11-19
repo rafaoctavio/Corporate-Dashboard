fb3dfc8fea9dad954712c55457f9ea1c4ddfeabe
rafaoctavio <rafaoctavio@gmail.com>
2020-11-19 17:26:13 -0300

    criando componentes sidebar e head do projeto

---
+++ my-app/src/components/Head.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Head() {
+    return(
+        <div>
+            <head>
+                <meta charset="utf-8"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                <title>Corporate Dashboard - Admin</title>
+
+                
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+                </link>
+                             
+            </head>
+        </div>
+    
+    );
+
+};
+
+export default Head;
