5825e3b5a5a21ad7c937e72d82c3df88104a8cf6
rafaoctavio <rafaoctavio@gmail.com>
2020-11-21 13:48:28 -0300

    corrigindo dependencias do projeto

--- src/components/BigCards.js
+++ src/components/BigCards.js
@@ -14,7 +14,7 @@ function BigCards () {
                 </div>
                 <div className="card-body">
                     <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25+'rem'}} src="assets/images/product_dummy.svg" alt="image dummy"></img>
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25+'rem'}} src="/product_dummy.svg" alt="product dummy"></img>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
                     <a target="_blank" rel="nofollow" href="/">View product detail</a>
